00a1be7b3e637ade7f18121e102b69469bf6c8ca
Arun Hari <Arun2.Hariharasubramanian@live.uwe.ac.uk>
2025-02-12 20:52:55 +0000

    Need to add ONNX Runtime if possible

---
+++ Advanced Tech Portfolio 2/Library/PackageCache/@de5d84acb3/scripts/modify-defines.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -xe
+
+PROJCET_DIR="$(cd "$(dirname "$0")/.." && pwd -P)"
+DEFINES_FILE="$PROJECT_DIR/scripts/UnityDefines.cs"
+SRC_ROOT="$PROJECT_DIR/com.github.asus4.onnxruntime/Runtime"
+
+concat_defines() {
+    cat "${DEFINES_FILE}" "$1" > "$1.tmp"
+    mv "$1.tmp" "$1"
+}
+
+concat_defines "${SRC_ROOT}/AssemblyInfo.shared.cs"
+concat_defines "${SRC_ROOT}/NativeMethods.shared.cs"
+concat_defines "${SRC_ROOT}/SessionOptions.shared.cs"
+
+echo "Done."
