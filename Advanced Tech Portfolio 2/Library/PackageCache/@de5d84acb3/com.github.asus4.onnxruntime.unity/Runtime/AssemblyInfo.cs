05f41202bd63a7992877cf958f54adab0eb9c2fa
Arun Hari <Arun2.Hariharasubramanian@live.uwe.ac.uk>
2025-02-26 20:23:35 +0000

    Going to move out this version of the project and add the version i made

--- Advanced Tech Portfolio 2/Library/PackageCache/@de5d84acb3/com.github.asus4.onnxruntime.unity/Runtime/AssemblyInfo.cs
+++
@@ -1,3 +0,0 @@
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("com.github.asus4.onnxruntime.unity.tests")]
--- Advanced Tech Portfolio 2/Library/PackageCache/com.unity.scripting.python@7.0.2/Documentation~/banner-message.md
+++
@@ -1,2 +0,0 @@
-> [!IMPORTANT]
-> Support for the Python Scripting package is scheduled to end as of Unity 6.1.
